8a41afd8c719787c7f868616964a5f3724dd3da8
2nter21 <155376343+2nter21@users.noreply.github.com>
2024-11-28 18:16:01 +0900

    [Chore] Create worker directory
    Create worker directory and initial file

---
+++ src/main/scala/project332/worker/Worker.scala
@@ -0,0 +1,15 @@
+package project332.worker
+
+import com.typesafe.scalalogging.LazyLogging
+import io.grpc.{Server, ServerBuilder}
+import scala.concurrent.{ExecutionContext, Future}
+
+object Worker {
+  def main(args: Array[String]): Unit = {
+    println("Hello world!")
+  }
+}
+
+class Worker {
+
+}
