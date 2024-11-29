4738b22a6ea769a66b470cdf92e9eadb04d15227
DevMizeKR <hwangck0827@gmail.com>
2024-11-29 17:50:08 +0900

    [Feat] gRPC Sample Code

--- src/main/scala/project332/worker/Worker.scala
+++ src/main/scala/project332/worker/Worker.scala
@@ -1,15 +1,50 @@
 package project332.worker
 
+import java.util.concurrent.TimeUnit
+import io.grpc.{ManagedChannel, ManagedChannelBuilder, StatusRuntimeException}
+import project332.example.{ExampleServiceGrpc, RequestMessage, ResponseMessage}
 import com.typesafe.scalalogging.LazyLogging
-import io.grpc.{Server, ServerBuilder}
-import scala.concurrent.{ExecutionContext, Future}
 
 object Worker {
+  def apply(host: String, port: Int): Worker = {
+    val channel = ManagedChannelBuilder.forAddress(host, port)
+      .usePlaintext()
+      .build()
+    val blockingStub = ExampleServiceGrpc.blockingStub(channel)
+    new Worker(channel, blockingStub)
+  }
+
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    val client = Worker("localhost", 50051)
+    try {
+      val user = args.headOption.getOrElse("world")
+      client.greet(user)
+    } finally {
+      client.shutdown()
+    }
   }
 }
 
-class Worker {
+class Worker private(
+                      private val channel: ManagedChannel,
+                      private val blockingStub: ExampleServiceGrpc.ExampleServiceBlockingStub
+                    ) extends LazyLogging {
 
+  // shutdown 메서드: gRPC 채널 종료
+  def shutdown(): Unit = {
+    channel.shutdown.awaitTermination(5, TimeUnit.SECONDS)
+  }
+
+  // greet 메서드: 서버로 요청을 보내고 응답을 받아오는 메서드
+  def greet(name: String): Unit = {
+    logger.info(s"Will try to greet $name ...")
+    val request = RequestMessage(name = name)
+    try {
+      val response = blockingStub.sayHello(request)
+      logger.info(s"Greeting: ${response.message}")
+    } catch {
+      case e: StatusRuntimeException =>
+        logger.warn("RPC failed: " + e.getStatus)
+    }
+  }
 }
