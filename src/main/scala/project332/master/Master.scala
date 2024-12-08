6d8d0fba42911f3d41301bde0a831b46c416ae8a
DevMizeKR <hwangck0827@gmail.com>
2024-12-08 12:44:24 +0900

    [Feat] Sampling Merged

--- src/main/scala/project332/master/Master.scala
+++ src/main/scala/project332/master/Master.scala
@@ -7,6 +7,7 @@ import scala.concurrent.{ExecutionContext, Future}
 
 import project332.common.Common.{findRandomPort, getLocalIP}
 import project332.connection.{CommunicateGrpc, ConnectionRequest, ConnectionResponse}
+import project332.connection.{SamplingRequest, SamplingResponse}
 
 object Master extends LazyLogging {
 
@@ -25,15 +26,19 @@ object Master extends LazyLogging {
 class Master(executionContext: ExecutionContext, val numClient: Int, val port: Int) extends LazyLogging {
   private[this] var server: Server = null
   private val clientLatch: CountDownLatch = new CountDownLatch(numClient)
+  var workers: Vector[WorkerClient] = Vector.empty
+  var data: List[Array[Byte]] = Nil
+  var count: Int = 0
 
   // 서버 시작
-  def start(): Unit = {
+  private def start(): Unit = {
     server = ServerBuilder.forPort(this.port)
       .addService(CommunicateGrpc.bindService(new CommunicateImpl, executionContext))
       .build()
       .start()
 
-    Master.logger.info(s"Server started at $getLocalIP : ${this.port}")
+    Master.logger.info(s"Client Number : $numClient")
+    Master.logger.info(s"Server started at $getLocalIP:${this.port}")
 
     sys.addShutdownHook {
       stop()
@@ -49,9 +54,26 @@ class Master(executionContext: ExecutionContext, val numClient: Int, val port: I
   }
 
   // 서버 종료 대기
-  def blockUntilShutdown(): Unit = {
+  private def blockUntilShutdown(): Unit = {
     if (server != null) {
       server.awaitTermination()
+    }
+  }
+
+  private def addData(data: String): Unit = {
+    this.synchronized {
+      count += 1
+      this.data :+ data.getBytes().grouped(10).toList
+      if (count == numClient)
+        Master.logger.info(s"Received All Data from $count clients.")
+    }
+  }
+
+  private def addWorker(ipAddress: String): Unit = {
+    this.synchronized {
+      this.workers = this.workers :+ new WorkerClient(this.workers.length, ipAddress)
+      if (this.workers.length == this.numClient)
+        Master.logger.info(s"${this.workers.mkString(", ")}")
     }
   }
 
@@ -65,5 +87,19 @@ class Master(executionContext: ExecutionContext, val numClient: Int, val port: I
       val reply = ConnectionResponse(isConnected = true, id = clientLatch.getCount.toInt)
       Future.successful(reply)
     }
+
+    override def sampling(req: SamplingRequest): Future[SamplingResponse] = {
+      Master.logger.info(s"Data from ${req.ipAddress}")
+      clientLatch.countDown()
+      addData(req.data)
+      clientLatch.await()
+
+      val reply = SamplingResponse(isChecked = true)
+      Future.successful(reply)
+    }
   }
+}
+
+class WorkerClient(val id: Int, val ip: String) {
+  override def toString: String = ip
 }
--- src/main/scala/project332/worker/Worker.scala
+++ src/main/scala/project332/worker/Worker.scala
@@ -1,70 +1,74 @@
 package project332.worker
 
 import com.typesafe.scalalogging.LazyLogging
-import io.grpc.{ManagedChannel, ManagedChannelBuilder}
+import io.grpc.{ManagedChannel, ManagedChannelBuilder, StatusRuntimeException}
+
+import scala.io.Source
 import scala.concurrent.{Await, Future, Promise}
 import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 import scala.concurrent.ExecutionContext.Implicits.global
-
 import project332.common.Common.getLocalIP
-import project332.connection.{CommunicateGrpc, ConnectionRequest, ConnectionResponse}
+import project332.connection.{CommunicateGrpc, ConnectionRequest, ConnectionResponse, SamplingRequest}
 
 object Worker extends LazyLogging {
   def main(args: Array[String]): Unit = {
-    if (args.length != 2) {
-      println("Usage: ./worker <Master-IP> <Master-Port>")
+    if (args.headOption.isEmpty || args.length != 1) {
+      println("Usage: ./worker <Master IP>:<Master Port>")
       System.exit(1)
     }
 
-    val masterIP = args(0)
-    val masterPort = args(1).toInt
+    val masterIP = args.headOption.get.split(':')
+    Worker.logger.info(s"Try to connect with Master : ${masterIP(0)}")
+    val client = Worker(masterIP(0), masterIP(1).toInt)
 
-    val worker = new Worker(masterIP, masterPort)
-    val done = worker.start()
-    Await.result(done, Duration.Inf)
-    worker.shutdown()
+    try { client.initialConnect() }
+    catch { case e: Exception => client.shutdown() }
+
+    val path = args.lastOption
+    if (path.isEmpty) { Worker.logger.info("File Path is Empty."); client.shutdown() }
+    else {
+      Worker.logger.info("Try to send Data to Master.")
+      val fileSource = Source.fromFile(args(1))
+      val keyList = fileSource.grouped(100).map(x => x.dropRight(90)).take(10000).toString()
+
+      try { client.sendData(keyList) }
+      finally { client.shutdown() }
+    }
+  }
+
+  def apply(ip: String, port: Int): Worker = {
+    val channel = ManagedChannelBuilder.forAddress(ip, port).usePlaintext().build()
+    val blockingStub = CommunicateGrpc.blockingStub(channel)
+    new Worker(channel, blockingStub)
   }
 }
 
-class Worker(masterIP: String, masterPort: Int) extends LazyLogging {
-  private val channel: ManagedChannel = ManagedChannelBuilder.forAddress(masterIP, masterPort)
-    .usePlaintext()
-    .build()
-  private val stub: CommunicateGrpc.CommunicateStub = CommunicateGrpc.stub(channel)
-  private val done: Promise[Boolean] = Promise[Boolean]()
-  private var id: Option[Int] = None
+class Worker(private val channel: ManagedChannel,
+             private val blockingStub: CommunicateGrpc.CommunicateBlockingStub
+            ) extends LazyLogging {
 
   def shutdown(): Unit = {
     channel.shutdown.awaitTermination(5, SECONDS)
   }
 
-  def start(): Future[Boolean] = {
-    connectToMaster()
-    done.future
-  }
-
-  private def connectToMaster(): Unit = {
+  def initialConnect(): Unit = {
     val request = ConnectionRequest(ipAddress = getLocalIP)
-    val response: Future[ConnectionResponse] = stub.connecting(request)
-
-    response.onComplete {
-      case Success(res) =>
-        handleConnectionResponse(res)
-      case Failure(exception) =>
-        logger.error(s"Connection to master failed: ${exception.getMessage}")
-        done.failure(exception)
+    try {
+      val response = blockingStub.connecting(request)
+      Worker.logger.info(s"Connection : ${response.isConnected}")
+    } catch {
+      case e: StatusRuntimeException => Worker.logger.warn(s"Failed : ${e.getStatus}")
     }
   }
 
-  private def handleConnectionResponse(response: ConnectionResponse): Unit = {
-    if (response.isConnected) {
-      this.id = Some(response.id)
-      logger.info(s"Connected to master with ID: ${response.id}")
-      done.success(true)
-    } else {
-      logger.error("Connection to master was rejected.")
-      done.failure(new Exception("Connection rejected"))
+  def sendData(data: String): Unit = {
+    val request = SamplingRequest(ipAddress = getLocalIP, data = data)
+    try {
+      val response = blockingStub.sampling(request)
+      Worker.logger.info(s"Data Sent : ${response.isChecked}")
+    } catch {
+      case e: StatusRuntimeException => Worker.logger.warn(s"Failed : ${e.getStatus}")
     }
   }
 }
