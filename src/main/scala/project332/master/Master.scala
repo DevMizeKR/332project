a530dd28f4f50a180b012a548c9bb4d5de329575
DevMizeKR <hwangck0827@gmail.com>
2024-12-08 20:26:45 +0900

    [Feat] Sampling / Partitioning Arbitrary Merged

--- src/main/scala/project332/master/Master.scala
+++ src/main/scala/project332/master/Master.scala
@@ -1,20 +1,30 @@
 package project332.master
 
 import com.typesafe.scalalogging.LazyLogging
+import com.google.protobuf.ByteString
+
 import java.util.concurrent.CountDownLatch
 import io.grpc.{Server, ServerBuilder}
-import scala.concurrent.{ExecutionContext, Future}
 
+import scala.collection.mutable.Map
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import project332.master.States._
 import project332.common.Common.{findRandomPort, getLocalIP}
+import project332.common.KeyOrdering
 import project332.connection.{CommunicateGrpc, ConnectionRequest, ConnectionResponse}
 import project332.connection.{SamplingRequest, SamplingResponse}
+import project332.connection.SamplingResponse.KeyRange
 
 object Master extends LazyLogging {
 
   private val randomPort: Int = findRandomPort
 
   def main(args: Array[String]): Unit = {
-    if (args.headOption.isEmpty) return
+    if (args.headOption.isEmpty) {
+      println("Usage: ./master <Client Number>")
+      System.exit(1)
+    }
 
     val port = args.find(arg => arg == "DEBUG").map(_ => 50052).getOrElse(randomPort)
     val server = new Master(ExecutionContext.global, args.headOption.get.toInt, port)
@@ -24,10 +34,12 @@ object Master extends LazyLogging {
 }
 
 class Master(executionContext: ExecutionContext, val numClient: Int, val port: Int) extends LazyLogging {
-  private[this] var server: Server = null
-  private val clientLatch: CountDownLatch = new CountDownLatch(numClient)
+  private[this] var server: Server = _
+  private var clientLatch: CountDownLatch = _
   private var workers: Vector[WorkerClient] = Vector.empty
-  var data: List[Array[Byte]] = Nil
+  var currentState: State = InitialState
+  var sampleData: List[Array[Byte]] = Nil
+  var idKeyRange: Map[Int, KeyRange] = Map.empty
   var count: Int = 0
 
   // 서버 시작
@@ -44,6 +56,8 @@ class Master(executionContext: ExecutionContext, val numClient: Int, val port: I
       stop()
       Master.logger.warn("Server shut down")
     }
+
+    stateTransition(ConnectingState)
   }
 
   // 서버 종료
@@ -60,47 +74,105 @@ class Master(executionContext: ExecutionContext, val numClient: Int, val port: I
     }
   }
 
-  private def addData(data: String): Unit = {
+  // Add New Worker to Master
+  private def addWorker(ipAddress: String): Int = {
     this.synchronized {
-      count += 1
-      this.data :+ data.getBytes().grouped(10).toList
-      if (count == numClient)
-        Master.logger.info(s"Received All Data from $count clients.")
+      val workerID = this.workers.length
+      this.workers = this.workers :+ new WorkerClient(this.workers.length, ipAddress)
+      if (this.workers.length == this.numClient) {
+        Master.logger.info(s"${this.workers.mkString(", ")}")
+        Future { stateTransition(SamplingState) }
+      }
+      return workerID
     }
   }
 
-  private def addWorker(ipAddress: String): Unit = {
+  private def stateTransition(nextState: State): Unit = {
+    Master.logger.info(s"Transition to $nextState.")
+    currentState = nextState
+    clientLatch.await()
+    clientLatch = new CountDownLatch(numClient)
+  }
+
+  // Add Sampled Data from Worker
+  private def addSampleData(id: Int, sampleData : Array[Byte]): Unit = {
     this.synchronized {
-      this.workers = this.workers :+ new WorkerClient(this.workers.length, ipAddress)
-      if (this.workers.length == this.numClient)
-        Master.logger.info(s"${this.workers.mkString(", ")}")
+      val worker = this.workers.find(_.id == id).get
+      worker.gotData = true
+
+      this.sampleData = this.sampleData ++ sampleData.grouped(10).toList
+      Master.logger.info(s"Received Data with length ${this.sampleData.length}.")
+      if (this.workers.count(_.gotData) == numClient) {
+        Master.logger.info(s"Received All Data from clients.")
+        createPivot()
+        Future { stateTransition(SortingState) }
+      }
     }
+  }
+
+  // Create Pivot for Partitioning
+  private def createPivot(): Unit = {
+    val minData: Array[Byte] = Array(Byte.MinValue, Byte.MinValue, Byte.MinValue, Byte.MinValue, Byte.MinValue, Byte.MinValue, Byte.MinValue, Byte.MinValue, Byte.MinValue, Byte.MinValue)
+    val maxData: Array[Byte] = Array(Byte.MaxValue, Byte.MaxValue, Byte.MaxValue, Byte.MaxValue, Byte.MaxValue, Byte.MaxValue, Byte.MaxValue, Byte.MaxValue, Byte.MaxValue, Byte.MaxValue)
+    val sortedData = this.sampleData.sorted(KeyOrdering)
+    val partition: Map[Int, (Array[Byte], Array[Byte])] = Map.empty
+    if (this.workers.length == 1) { partition.put(workers(0).id, (minData, maxData))}
+    else {
+      val range = sortedData.length / this.workers.length
+      var loop = 0
+
+      for (worker <- workers.toList) {
+        val startIndex = loop * range
+        val endIndex = Math.min((loop + 1) * range, sortedData.length)
+
+        if (loop == 0) {
+          val bytes = sortedData(endIndex - 1).clone()
+          bytes.update(9, bytes(9).-(1).toByte)
+          partition.put(worker.id, (minData, bytes))
+        } else if (loop == this.workers.length - 1 || endIndex == sortedData.length) {
+          partition.put(worker.id, (sortedData(startIndex), maxData))
+        } else {
+          val bytes = sortedData(endIndex - 1).clone()
+          bytes.update(9, bytes(9).-(1).toByte)
+          partition.put(worker.id, (sortedData(startIndex), bytes))
+        }
+        loop += 1
+      }
+    }
+
+    idKeyRange = partition.map(x=>(x._1, KeyRange(lowerBound = ByteString.copyFrom(x._2._1), upperBound = ByteString.copyFrom(x._2._2))))
   }
 
   // gRPC 서비스 구현
   private class CommunicateImpl extends CommunicateGrpc.Communicate {
     override def connecting(req: ConnectionRequest): Future[ConnectionResponse] = {
       Master.logger.info(s"Handshake from ${req.ipAddress}")
+
+      val workerID = addWorker(req.ipAddress)
       clientLatch.countDown()
-      addWorker(req.ipAddress)
       clientLatch.await()
 
-      val reply = ConnectionResponse(isConnected = true, id = clientLatch.getCount.toInt)
+      val reply = ConnectionResponse(isConnected = true, id = workerID)
       Future.successful(reply)
     }
 
     override def sampling(req: SamplingRequest): Future[SamplingResponse] = {
-      Master.logger.info(s"Data from ${req.ipAddress}")
+      addSampleData(req.id, req.data.toByteArray)
       clientLatch.countDown()
-      addData(req.data)
       clientLatch.await()
 
-      val reply = SamplingResponse(isChecked = true)
+      val reply = SamplingResponse(isChecked = true, idKeyRange = idKeyRange.toMap)
       Future.successful(reply)
     }
   }
 }
 
 class WorkerClient(val id: Int, val ip: String) {
+  var gotData: Boolean = false
   override def toString: String = ip
+}
+
+object States extends Enumeration {
+  type State = Value
+  val InitialState, ConnectingState, SamplingState, SortingState, ShufflingState, MergingState, EndState = Value
 }
--- src/main/scala/project332/worker/Worker.scala
+++ src/main/scala/project332/worker/Worker.scala
@@ -1,52 +1,62 @@
 package project332.worker
 
 import com.typesafe.scalalogging.LazyLogging
-import io.grpc.{ManagedChannel, ManagedChannelBuilder, StatusRuntimeException}
+import com.google.protobuf.ByteString
+import io.grpc.{ManagedChannel, ManagedChannelBuilder}
 
+import java.io.File
 import scala.io.Source
-import scala.concurrent.{Await, Future, Promise}
-import scala.concurrent.duration._
 import scala.util.{Failure, Success}
+import scala.collection.mutable
+import scala.collection.mutable.Map
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future, Promise}
 import scala.concurrent.ExecutionContext.Implicits.global
 import project332.common.Common.getLocalIP
-import project332.connection.{CommunicateGrpc, ConnectionRequest, ConnectionResponse, SamplingRequest}
+import project332.connection.{CommunicateGrpc, ConnectionRequest, ConnectionResponse, SamplingRequest, SamplingResponse}
+import project332.connection.SamplingResponse.KeyRange
 
 object Worker extends LazyLogging {
+  def apply(ip: String, port: Int, inputDirectories: Array[String], outputDirectory: String): Worker = {
+    val channel = ManagedChannelBuilder.forAddress(ip, port).usePlaintext().build()
+    val stub = CommunicateGrpc.stub(channel)
+    new Worker(channel, stub, inputDirectories, outputDirectory)
+  }
+
   def main(args: Array[String]): Unit = {
     if (args.headOption.isEmpty || args.length != 1) {
-      println("Usage: ./worker <Master IP>:<Master Port>")
+      println("Usage: ./worker <Master IP>:<Master Port> <Input Number> <Input Directory Lists> <Output Directory>")
       System.exit(1)
     }
 
     val masterIP = args.headOption.get.split(':')
+    val inputCount = args(1).toInt
+    val inputDirectories: Array[String] = args.slice(2, 2 + inputCount)
+    val outputDirectory = args.lastOption.get
+
     Worker.logger.info(s"Try to connect with Master : ${masterIP(0)}")
-    val client = Worker(masterIP(0), masterIP(1).toInt)
 
-    try { client.initialConnect() }
-    catch { case e: Exception => client.shutdown() }
 
-    val path = args.lastOption
-    if (path.isEmpty) { Worker.logger.info("File Path is Empty."); client.shutdown() }
-    else {
-      Worker.logger.info("Try to send Data to Master.")
-      val fileSource = Source.fromFile(args(0))
-      val keyList = fileSource.grouped(100).map(x => x.dropRight(90)).take(10000).toString()
-
-      try { client.sendData(keyList) }
-      finally { client.shutdown() }
-    }
-  }
-
-  def apply(ip: String, port: Int): Worker = {
-    val channel = ManagedChannelBuilder.forAddress(ip, port).usePlaintext().build()
-    val blockingStub = CommunicateGrpc.blockingStub(channel)
-    new Worker(channel, blockingStub)
+    val client = Worker(masterIP(0), masterIP(1).toInt, inputDirectories, outputDirectory)
+    val done = client.start()
+    Await.result(done, Duration.Inf)
+    client.shutdown()
   }
 }
 
 class Worker(private val channel: ManagedChannel,
-             private val blockingStub: CommunicateGrpc.CommunicateBlockingStub
+             private val stub: CommunicateGrpc.CommunicateStub,
+             private val inputDirectories: Array[String],
+             private val outputDirectory: String
             ) extends LazyLogging {
+  val done: Promise[Boolean] = Promise[Boolean]()
+  var idKeyRange: mutable.Map[Int, KeyRange] = mutable.Map.empty
+  var id: Int = 0
+
+  def start(): Future[Boolean] = {
+    Future { initialConnect() }
+    done.future
+  }
 
   def shutdown(): Unit = {
     channel.shutdown.awaitTermination(5, SECONDS)
@@ -54,21 +64,41 @@ class Worker(private val channel: ManagedChannel,
 
   def initialConnect(): Unit = {
     val request = ConnectionRequest(ipAddress = getLocalIP)
-    try {
-      val response = blockingStub.connecting(request)
-      Worker.logger.info(s"Connection : ${response.isConnected}")
-    } catch {
-      case e: StatusRuntimeException => Worker.logger.warn(s"Failed : ${e.getStatus}")
+    val response = stub.connecting(request)
+    response.onComplete {
+      case Success(value) =>
+        Worker.logger.info(s"Connection : ${value.isConnected}")
+        Worker.logger.info(s"Worker ID : ${value.id}")
+        this.id = value.id
+        val sampleData = getSampleData
+        sendSampleData(sampleData)
+      case Failure(exception) => Worker.logger.warn(s"Failed : $exception")
     }
+    Await.ready(response, 10.seconds)
   }
 
-  def sendData(data: String): Unit = {
-    val request = SamplingRequest(ipAddress = getLocalIP, data = data)
-    try {
-      val response = blockingStub.sampling(request)
-      Worker.logger.info(s"Data Sent : ${response.isChecked}")
-    } catch {
-      case e: StatusRuntimeException => Worker.logger.warn(s"Failed : ${e.getStatus}")
+  def getSampleData: Array[Byte] = {
+    val data = inputDirectories.map(new File(_))
+      .flatMap(_.listFiles.filter(_.isFile))
+      .map(Source.fromFile(_))
+      .flatMap(_.grouped(100).toList.map(x => x.dropRight(90)).take(10000).flatMap(x => x.map(y => y.toByte)).toArray)
+    Worker.logger.info(s"Data : ${data.mkString("Array(", ", ", ")")}")
+    data
+  }
+
+  def sendSampleData(data: Array[Byte]): Unit = {
+    val request = SamplingRequest(id = this.id, data = ByteString.copyFrom(data))
+    val response = stub.sampling(request)
+
+    response.onComplete {
+      case Success(value) =>
+        if (value.isChecked) {
+          this.idKeyRange = mutable.Map.from(value.idKeyRange.map(x => (x._1, new KeyRange(x._2.lowerBound, x._2.upperBound))))
+          Worker.logger.info(s"Data : $value")
+        } else {
+          Worker.logger.warn("Partitioning Failed.")
+        }
+      case Failure(exception) => Worker.logger.warn(s"RPC failed: $exception")
     }
   }
 }
