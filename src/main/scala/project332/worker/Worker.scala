4848c584e0556ad790e37715846950e297dc6d95
DevMizeKR <hwangck0827@gmail.com>
2024-12-08 21:55:57 +0900

    [Feat] Sorting Merged

--- src/main/scala/project332/worker/Worker.scala
+++ src/main/scala/project332/worker/Worker.scala
@@ -4,15 +4,16 @@ import com.typesafe.scalalogging.LazyLogging
 import com.google.protobuf.ByteString
 import io.grpc.{ManagedChannel, ManagedChannelBuilder}
 
-import java.io.File
+import java.io.{File, FileOutputStream}
 import scala.io.Source
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Random}
 import scala.collection.mutable
 import scala.collection.mutable.Map
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future, Promise}
 import scala.concurrent.ExecutionContext.Implicits.global
 import project332.common.Common.getLocalIP
+import project332.common.KeyOrdering
 import project332.connection.{CommunicateGrpc, ConnectionRequest, ConnectionResponse, SamplingRequest, SamplingResponse}
 import project332.connection.SamplingResponse.KeyRange
 
@@ -62,7 +63,7 @@ class Worker(private val channel: ManagedChannel,
     channel.shutdown.awaitTermination(5, SECONDS)
   }
 
-  def initialConnect(): Unit = {
+  private def initialConnect(): Unit = {
     val request = ConnectionRequest(ipAddress = getLocalIP)
     val response = stub.connecting(request)
     response.onComplete {
@@ -77,16 +78,15 @@ class Worker(private val channel: ManagedChannel,
     Await.ready(response, 10.seconds)
   }
 
-  def getSampleData: Array[Byte] = {
+  private def getSampleData: Array[Byte] = {
     val data = inputDirectories.map(new File(_))
       .flatMap(_.listFiles.filter(_.isFile))
       .map(Source.fromFile(_))
-      .flatMap(_.grouped(100).toList.map(x => x.dropRight(90)).take(10).flatMap(x => x.map(y => y.toByte)).toArray)
-    Worker.logger.info(s"Data : ${data.mkString("Array(", ", ", ")")}")
+      .flatMap(_.grouped(100).toList.map(x => x.dropRight(90)).take(10000).flatMap(x => x.map(y => y.toByte)).toArray)
     data
   }
 
-  def sendSampleData(data: Array[Byte]): Unit = {
+  private def sendSampleData(data: Array[Byte]): Unit = {
     val request = SamplingRequest(id = this.id, data = ByteString.copyFrom(data))
     val response = stub.sampling(request)
 
@@ -94,10 +94,53 @@ class Worker(private val channel: ManagedChannel,
       case Success(value) =>
         if (value.isChecked) {
           this.idKeyRange = mutable.Map.from(value.idKeyRange.map(x => (x._1, new KeyRange(x._2.lowerBound, x._2.upperBound))))
+          sortingData()
         } else {
           Worker.logger.warn("Partitioning Failed.")
         }
       case Failure(exception) => Worker.logger.warn(s"RPC failed: $exception")
     }
+  }
+
+  // Sorting Data with Keys
+  private def sortingData(): Unit = {
+    val data = inputDirectories.map(new File(_))
+      .flatMap(_.listFiles.filter(_.isFile))
+      .map(Source.fromFile(_))
+      .toList
+
+    for (d <- data) {
+      val result = d.grouped(100).toList.map(line => {
+        val (key, value) = line.splitAt(10)
+        (key.map(_.toByte).toArray, value.map(_.toByte).toArray)
+      }).sortBy(_._1)(KeyOrdering)
+      saveSortedData(result)
+    }
+  }
+
+  // Save Sorted Data
+  private def saveSortedData(result: List[(Array[Byte], Array[Byte])]): Unit = {
+    val iter = idKeyRange.iterator
+    var (id, keyRange) = iter.next()
+
+    def createNewFile(): FileOutputStream = {
+      val fileName = s"$outputDirectory/worker${id}_${Random.alphanumeric.take(10).mkString}"
+      new FileOutputStream(new File(fileName))
+    }
+
+    var outputStream = createNewFile()
+
+    for (rs <- result) {
+      if (KeyOrdering.compare(rs._1, keyRange.upperBound.toByteArray) > 0) {
+        outputStream.close()
+        val (newId, newKeyRange) = iter.next()
+        id = newId
+        keyRange = newKeyRange
+        outputStream = createNewFile()
+      }
+      outputStream.write(rs._1)
+      outputStream.write(rs._2)
+    }
+    outputStream.close()
   }
 }
